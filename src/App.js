bf27927847602d6c3b4d8d8a782d7fcf8552d7b8
Benjamin Huy Pham <benjaminhpham@gmail.com>
2023-08-11 14:41:06 -0700

    first lesson

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Counter from "./components/Counter";
 
 function App() {
+  const [toggleCounter, setToggleCounter] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setToggleCounter((prevState) => !prevState)}>
+        Show Counter
+      </button>
+      <Counter />
+      {toggleCounter && <Counter />}
     </div>
   );
 }
