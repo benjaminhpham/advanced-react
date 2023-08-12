638067d87bad808e9e9a4acc076abf970ecbfe9c
Benjamin Huy Pham <benjaminhpham@gmail.com>
2023-08-11 18:26:58 -0700

    lesson-6: spread syntax + spreading in React

--- src/lessons/lesson-5/App5.jsx
+++ src/lessons/lesson-5/App5.jsx
@@ -7,6 +7,20 @@ const employees = [
 ];
 
 export default function App5() {
+  const arr1 = [0, 1, 2];
+  const arr2 = [3, 4, 5];
+  const arr3 = [...arr1, ...arr2];
+
+  const baseEmployeeObject = {
+    role: "employee",
+    company: "CodeSandbox",
+  };
+
+  const firstEmployee = { ...baseEmployeeObject, ...employees[0] };
+
+  console.log(arr3);
+  console.log(firstEmployee);
+
   return (
     <div>
       {employees.map(({ name, id }, index) => (
